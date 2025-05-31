0e034398ce3a577e421cde3181a62f315d4cd715
decobwebs <cobwebb784@gmail.com>
2025-05-31 02:06:44 -1200

    Vercel version

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const cards = document.querySelectorAll('.card');
+  cards.forEach(card => {
+    card.addEventListener('mouseenter', () => {
+      card.style.transform = 'translateY(-10px)';
+    });
+
+    card.addEventListener('mouseleave', () => {
+      card.style.transform = 'translateY(0)';
+    });
+  });
+});
